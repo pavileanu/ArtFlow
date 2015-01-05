b6efe28ce107b6dca31dc35148e5074b76a98ee7
pavi <mirceashark2007@yahoo.com>
2015-01-05 11:19:26 +0200

    Unknown commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112113134) do
+ActiveRecord::Schema.define(version: 20141113151523) do
 
   create_table "campaigns", force: true do |t|
     t.datetime "created_at"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20141112113134) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
